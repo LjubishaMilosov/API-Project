14a4a95f3f16942ae7e9ee39466b29e691ee2b7d
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-09 22:37:53 +0800

    few simple tests

--- e2e/src/tests/GET.test.js
+++ e2e/src/tests/GET.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { expect, request, test } from '@playwright/test'
 
 // test('retrieve user posts', async ({request}) => {
 //     const response = await request.get('/posts')
@@ -10,4 +10,14 @@ test('retrieve user posts', async ({ request }) => {
         const response = await request.get('/posts')
         expect(response.ok()).toBeTruthy()
         expect(response.status()).toBe(200)
+    })
+
+    test('retrieve user post', async ({request}) => {
+        const response = await request.get('/posts/1')
+        expect(response.ok()).toBeTruthy()
+        expect(response.status()).toBe(200)
+        expect(await response.json()).toEqual(expect.objectContaining({
+            "id":1,
+            "userId":1
+        }))
     })
