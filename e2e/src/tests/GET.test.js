f37202da594eac166e3d85f1575c4c5507de3e83
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-09 21:58:48 +0800

    initialization and first test

---
+++ e2e/src/tests/GET.test.js
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test'
+
+// test('retrieve user posts', async ({request}) => {
+//     const response = await request.get('/posts')
+//     expect(response.ok()).toBeTruthy()
+//     expect(response.status()).toBe(200)
+// })
+
+test('retrieve user posts', async ({ request }) => {
+        const response = await request.get('/posts')
+        expect(response.ok()).toBeTruthy()
+        expect(response.status()).toBe(200)
+    })
