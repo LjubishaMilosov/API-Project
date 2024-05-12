7bb2e9ada30d94066079db393853ae847e515337
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-12 20:32:44 +0200

    introducing Cucumber World - part 2

---
+++ e2e/src/step-definitions/setup/hooks.ts
@@ -0,0 +1,10 @@
+import { Before } from '@cucumber/cucumber'
+import { ScenarioWorld } from './world'
+
+Before(async function (this: ScenarioWorld, scenario) {
+    console.log(`Running cucumber scenario ${scenario.pickle.name}`)
+
+    const ready = await this.init()
+    return ready
+    
+})
