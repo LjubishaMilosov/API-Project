37f91d78ab35d6c97baa453ab23d8659b749fb17
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-13 03:02:36 +0800

    passing our API response to cucumber steps with Cucumber World - part 2

--- e2e/src/step-definitions/assertions/response-steps.ts
+++ e2e/src/step-definitions/assertions/response-steps.ts
@@ -1,5 +1,6 @@
 import { Then } from '@cucumber/cucumber'
 import { ScenarioWorld } from '../setup/world'
+import { expect } from 'playwright/test'
 
 Then(
     /^the response was (successful)?(unsuccessful)?$/,
@@ -9,7 +10,15 @@ Then(
             globalAPIResponseVariables
         } = this
 
-        console.log(await globalAPIResponseVariables.response.text())
+        console.log(`the response was ${unsuccessful?'unsuccessful ':'successful '} `)
+
+        const response = globalAPIResponseVariables.response
+
+        if(unsuccessful) {
+            expect(response.ok()).toBeFalsy()
+        }else{
+            expect(response.ok()).toBeTruthy()
+        }
     }
 
-)
+) 
